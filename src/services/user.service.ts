688a566424cb5dccbf32a0a7615898050bb77d8f
이정건 <jklee9041@gmail.com>
2022-07-18 23:18:40 +0900

    feat: 중복가입 방지 로직 작성(검사를 위한 코드 업로드)

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { PhoneNumberEntity } from '@root/entities/phone-number.entity';
 import { UserRepository } from '@root/entities/repositories/user.repository';
 import { UserEntity } from '@root/entities/user.entity';
 
@@ -16,6 +15,19 @@ export class UserService {
   }
 
   async saveUser(user: UserEntity) {
+    // email 중복가입 방지
+    let isNotDuplicated;
+    try { // 전달받은 이메일 조회
+      isNotDuplicated = UserEntity.findOne({where: {email: user.email}})
+      console.log("flow: try")
+    }catch { // 중복 시 403 Exception 발생
+      console.log("flow: catch")
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'This email is already joined'
+      }, HttpStatus.FORBIDDEN)
+    }
+
     return await this.userRepository.save(user)
   }
 }
