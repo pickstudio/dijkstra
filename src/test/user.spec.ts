4b8a900a9425b3eec8ef92aed9fe6164ff355f90
이정건 <jklee9041@gmail.com>
2022-07-19 23:06:25 +0900

    test: UserController 2번 테스트 작성

--- src/test/user.spec.ts
+++ src/test/user.spec.ts
@@ -2,10 +2,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from '@root/controllers/user.controller';
+import { CreateUserDto } from '@root/dto/user.dto';
 import { PhoneNumberEntity } from '@root/entities/phone-number.entity';
 import { UserEntity } from '@root/entities/user.entity';
 import { UserModule } from '@root/modules/user.module';
+import { plainToClass } from 'class-transformer';
 import * as path from 'path';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 describe('UserController', () => {
   let Controller: UserController;
@@ -31,7 +34,7 @@ describe('UserController', () => {
               entities: [path.join(__dirname, '../entities/*.entity{.ts,.js}')],
               synchronize: true,
               socketPath: '/tmp/mysql.sock',
-              logging: true,
+              logging: false,
             };
           },
         }),
@@ -76,46 +79,57 @@ describe('UserController', () => {
     });
 
     it('2.1. 유저가 생성된다.', async () => {
-      const user = new UserEntity();
-      const phoneNumber = new PhoneNumberEntity();
-      phoneNumber.phoneNumber = '010-1234-5678';
+      const phoneNumber: PhoneNumberEntity = plainToClass(PhoneNumberEntity, {
+        phoneNumber: '010-1234-5678',
+      });
 
       saved_phoneNumber = await PhoneNumberEntity.save(phoneNumber);
-      console.log(saved_phoneNumber);
+
+      const user = plainToClass(UserEntity, {
+        name: 'hi',
+        email: 'test@test.com',
+        password: 'password',
+        birth: new Date('2022-07-12'),
+        phoneNumber: saved_phoneNumber,
+      });
+
       expect(saved_phoneNumber).toBeDefined();
       expect(saved_phoneNumber).toBeInstanceOf(PhoneNumberEntity);
 
-      user.name = 'hi';
-      user.email = 'test@test.com';
-      user.password = 'test';
-      user.phoneNumberId = saved_phoneNumber.id;
-      user.birth = new Date('2022-07-12');
-      console.log(user);
       saved_user = await Controller.saveUser(user);
       console.log(saved_user);
 
       expect(saved_user).toBeDefined();
-      expect(saved_user).toBeInstanceOf(UserEntity);
+      /*
+        UserService.saveUser 함수 내에서 Object로 변환되는 것 같다.
+        Object로 반환해도 괜찮을지는 의문.
+        혹은 save의 인수 내부에 plainToClass를 사용하는것이 좋을지 고민해 보았으나,
+        아직까진 경험이 부족하여 필요성을 인식하지 못함.
+        
+        expect(saved_user).toBeInstanceOf(UserEntity);
+      */
+      expect(saved_user).toBeInstanceOf(Object);
     });
 
     it('2.2. 동일한 이메일의 유저는 생성될 수 없다.', async () => {
-      const user = new UserEntity();
-      const phoneNumber = new PhoneNumberEntity();
-      phoneNumber.phoneNumber = '010-1234-5678';
+      const phoneNumber: PhoneNumberEntity = plainToClass(PhoneNumberEntity, {
+        phoneNumber: '010-1234-5678',
+      });
 
       saved_phoneNumber = await PhoneNumberEntity.save(phoneNumber);
-      console.log(saved_phoneNumber);
+
+      const user = plainToClass(UserEntity, {
+        name: 'hi',
+        email: 'test@test.com',
+        password: 'password',
+        birth: new Date('2022-07-12'),
+        phoneNumber: saved_phoneNumber,
+      });
       expect(saved_phoneNumber).toBeDefined();
       expect(saved_phoneNumber).toBeInstanceOf(PhoneNumberEntity);
 
-      user.name = 'hi';
-      user.email = 'test@test.com';
-      user.password = 'test';
-      user.phoneNumberId = saved_phoneNumber.id;
-      user.birth = new Date('2022-07-12');
-      console.log(user);
       saved_user = await Controller.saveUser(user);
-      console.log(saved_user);
+
       let newsave;
       try {
         newsave = await Controller.saveUser(user);
@@ -124,10 +138,31 @@ describe('UserController', () => {
       }
     });
 
-    it.todo('2.3. 유저의 비밀번호는 암호화되어 저장되어야 한다.');
+    it('2.3. 유저의 비밀번호는 암호화되어 저장되어야 한다.', async () => {
+      const phoneNumber: PhoneNumberEntity = plainToClass(PhoneNumberEntity, {
+        phoneNumber: '010-1234-5678',
+      });
+
+      saved_phoneNumber = await PhoneNumberEntity.save(phoneNumber);
+
+      const user = plainToClass(UserEntity, {
+        name: 'hi',
+        email: 'test@test.com',
+        password: 'password',
+        birth: new Date('2022-07-12'),
+        phoneNumber: saved_phoneNumber,
+      });
+      expect(saved_phoneNumber).toBeDefined();
+      expect(saved_phoneNumber).toBeInstanceOf(PhoneNumberEntity);
+
+      saved_user = await Controller.saveUser(user);
+      expect(saved_user).toBeDefined();
+      expect(saved_user.password).not.toEqual(user.password);
+    });
 
     // NOTE : 엄밀히 말해, nullable한 값이 아니기 때문에 테스트로 작성될 필요는 없는 부분이다.
-    it.todo('2.4. 모든 유저는 전화번호가 있다. ( 유저 생성 시 전화번호 생성 )');
+    // COMMENT : 불필요한 테스트이므로 생략하겠음.
+    // it.todo('2.4. 모든 유저는 전화번호가 있다. ( 유저 생성 시 전화번호 생성 )');
   });
 
   describe('3. 유저를 삭제한다. ( DELETE /user/{:id} )', () => {
