e60753afa218ae9e30b90f368483963dcafc908c
이정건 <jklee9041@gmail.com>
2022-07-26 20:20:39 +0900

    feat: logger 요청 정보 console로 출력하도록 수정

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -11,13 +11,28 @@ import { tap } from 'rxjs/operators';
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // NOTE : 요청이 들어오는 시점
-    console.log('Before...');
-    const now = Date.now();
+    // console.log('Before...');
+    const now: Date = new Date();
+    const request = context.switchToHttp().getRequest();
+    const ip = request.ip;
+    const user_agent = request.get('user-agent') || '';
+
+    const method = request.method;
+    const url = request.originalUrl;
+    const http = request.get('h');
 
     return next.handle().pipe(
       tap(() => {
         // NOTE : 응답이 나가는 시점
-        console.log(`After... ${Date.now() - now}ms`);
+        const now2: Date = new Date();
+        const delay = now2.getTime() - now.getTime();
+        const response = context.switchToHttp().getResponse();
+        const content_length = response.get('content-length');
+        console.log(
+          `${ip} - [${now.toISOString()}] ${method} ${url} | ${
+            response.statusCode
+          } - ${content_length} | ${user_agent} +${delay}ms`,
+        );
       }),
     );
   }
