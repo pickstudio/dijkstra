e2f208316c760cf3afab80122d74522d3e86589a
이정건 <jklee9041@gmail.com>
2022-07-26 15:00:07 +0900

    feat: addressBook Entity 및 Dto 일부 수정

--- src/dto/address-book.dto.ts
+++ src/dto/address-book.dto.ts
@@ -1,9 +1,16 @@
-import { IsString } from "@nestjs/class-validator"
-import { PhoneNumberEntity } from "@root/entities/phone-number.entity";
-import { OneToOne } from "typeorm";
+import { IsInstance, IsString } from '@nestjs/class-validator';
+import { PhoneNumberEntity } from '@root/entities/phone-number.entity';
+import { OneToOne } from 'typeorm';
 
+export class OneAddressDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  phone: string;
+}
 
 export class AddressBookDto {
-    @IsString({each: true})
-    addressBook: string[];
+  @IsInstance(OneAddressDto, { each: true })
+  addressBook: OneAddressDto[];
 }
--- src/entities/address-book.entity.ts
+++ src/entities/address-book.entity.ts
@@ -1,15 +1,19 @@
-import { IsInt } from "@nestjs/class-validator";
-import { Column, Entity, PrimaryColumn, Unique } from "typeorm";
-import { PhoneNumberEntity } from "./phone-number.entity";
+import { IsInt, IsString } from '@nestjs/class-validator';
+import { Column, Entity, PrimaryColumn, Unique } from 'typeorm';
+import { PhoneNumberEntity } from './phone-number.entity';
 
 @Entity()
 // @Unique(['userId', 'phoneNumberId'])
 export class UserHasPhoneNumberEntity {
-    @IsInt()
-    @PrimaryColumn()
-    userId: number;
+  @IsInt()
+  @PrimaryColumn()
+  userId: number;
 
-    @IsInt()
-    @PrimaryColumn()
-    phoneNumberId: number
+  @IsInt()
+  @PrimaryColumn()
+  phoneNumberId: number;
+
+  @IsString()
+  @Column({ nullable: false })
+  phoneNickname: string;
 }
