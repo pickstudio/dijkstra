31a28f7fc0f435f42dc5adafa6d4bc7e734e8261
이정건 <jklee9041@gmail.com>
2022-07-29 01:17:25 +0900

    fix: Dto 클래스명 수정 및 잘못된 메서드 선언 제거

--- src/dto/common-get-page-param.dto.ts
+++ src/dto/common-get-page-param.dto.ts
@@ -2,19 +2,12 @@ import { IsInt } from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { NumberRange } from '@root/decorators/number-range.decorator';
 
-export class CommonGetPageParamDto {
+export class PageParamDto {
   @ApiProperty({})
   @IsInt()
   page: number;
 
   @ApiProperty({})
   @NumberRange(1, 100)
-  limit: number;
-
-  toPageOption = () => {
-    return {
-      skip: (this.page - 1) * this.limit,
-      take: this.limit,
-    };
-  };
+  take: number;
 }
