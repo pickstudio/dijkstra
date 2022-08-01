239e3d00a149bf89e6607c0dd34981ff63218800
이정건 <jklee9041@gmail.com>
2022-08-01 20:03:44 +0900

    test: auth 컨트롤러 및 서비스 spec파일 작성(진행 중)

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,60 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from '@root/modules/user.module';
 import { AuthController } from './auth.controller';
+import { AuthModule } from './auth.module';
+import { AuthService } from './auth.service';
+import * as path from 'path';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
+import { UserRepository } from '@root/entities/repositories/user.repository';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: `.env.${process.env.NODE_ENV}`,
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => {
+            return {
+              type: 'mysql',
+              host: 'localhost',
+              port: 3306,
+              username: configService.get('DB_USERNAME'),
+              password: configService.get('DB_PASSWORD'),
+              database: configService.get('DB_DATABASE'),
+              entities: [path.join(__dirname, '../entities/*.entity{.ts,.js}')],
+              synchronize: true,
+              socketPath: '/tmp/mysql.sock',
+              logging: false,
+            };
+          },
+        }),
+        PassportModule,
+        JwtModule.registerAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get('JWT_SECRET'),
+            signOptions: {
+              expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+            },
+          }),
+        }),
+        AuthModule,
+        UserModule,
+      ],
+      providers: [AuthService, LocalStrategy, JwtStrategy, UserRepository],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,14 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '@root/entities/user.entity';
+import { UserModule } from '@root/modules/user.module';
+import { plainToClass } from 'class-transformer';
+import * as path from 'path';
+import * as bcrypt from 'bcrypt';
+import { LoginInfo } from './auth.input';
+import { AuthModule } from './auth.module';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -6,13 +16,78 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        AuthModule,
+        UserModule,
+        JwtModule.registerAsync({
+          imports: [
+            ConfigModule.forRoot({
+              isGlobal: true,
+              envFilePath: `.env.${process.env.NODE_ENV}`,
+            }),
+          ],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.get('JWT_SECRET'),
+            signOptions: {
+              expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+            },
+          }),
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => {
+            return {
+              type: 'mysql',
+              host: 'localhost',
+              port: 3306,
+              username: configService.get('DB_USERNAME'),
+              password: configService.get('DB_PASSWORD'),
+              database: configService.get('DB_DATABASE'),
+              entities: [path.join(__dirname, '../entities/*.entity{.ts,.js}')],
+              synchronize: true,
+              socketPath: '/tmp/mysql.sock',
+              logging: false,
+            };
+          },
+        }),
+      ],
       providers: [AuthService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('0. 테스트 준비', () => {
+    it('Service is defined', () => {
+      expect(service).toBeDefined();
+    });
+  });
+
+  describe('1. Local 로그인.', () => {
+    let loginInfo: LoginInfo;
+    beforeEach(() => {
+      loginInfo = plainToClass(LoginInfo, {
+        email: 'test3@test.com',
+        password: 'password123!@#',
+      });
+    });
+    it('1.1. 이메일과 패스워드를 정확히 입력하면 validateUser 함수가 유저 객체를 반환한다.', async () => {
+      const state = await service.validateUser(
+        loginInfo.email,
+        loginInfo.password,
+      );
+      expect(state).toBeDefined();
+    });
+    it('1.2. 로그인에 실패하면 오류를 반환한다.', async () => {
+      let state;
+      try {
+        state = await service.validateUser(loginInfo.email, 'asdf');
+        expect(state).toBeDefined();
+      } catch {
+        expect(state).toBeUndefined();
+      }
+    });
   });
 });
