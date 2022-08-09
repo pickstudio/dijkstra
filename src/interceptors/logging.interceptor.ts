e5266bf70671efb0e9120795ecfc4db26ce71f2a
kakasoo <kscodebase@gmail.com>
2022-08-10 00:40:21 +0900

    chore : logging.interceptor에 대한 코드 정리

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -11,27 +11,28 @@ import { tap } from 'rxjs/operators';
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     // NOTE : 요청이 들어오는 시점
-    // console.log('Before...');
-    const now: Date = new Date();
+    const startPoint: Date = new Date();
     const request = context.switchToHttp().getRequest();
+
     const ip = request.ip;
     const user_agent = request.get('user-agent') || '';
 
     const method = request.method;
     const url = request.originalUrl;
-    const http = request.get('h');
 
     return next.handle().pipe(
       tap(() => {
         // NOTE : 응답이 나가는 시점
-        const now2: Date = new Date();
-        const delay = now2.getTime() - now.getTime();
+        const endPoint: Date = new Date();
+        const delay = endPoint.getTime() - startPoint.getTime();
+
         const response = context.switchToHttp().getResponse();
-        const content_length = response.get('content-length');
+        const contentLength = response.get('content-length') || 0;
+
         console.log(
-          `${ip} - [${now.toISOString()}] ${method} ${url} | ${
+          `${ip} - [${startPoint.toISOString()}]${method} ${url} | ${
             response.statusCode
-          } - ${content_length} | ${user_agent} +${delay}ms`,
+          } - ${contentLength} | ${user_agent} +${delay}ms`,
         );
       }),
     );
