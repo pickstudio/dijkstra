45f63c63c94754e2de7cd04efce5b5045b0bac7f
kakasoo <kscodebase@gmail.com>
2022-08-13 00:54:16 +0900

    style : 컨벤션 정리
    - 클랙스 명은 대문자로 시작, 인스턴스 명은 소문자로 시작해야 합니다.

--- src/dto/common/search-pagination.dto.ts
+++ src/dto/common/search-pagination.dto.ts
@@ -1,10 +1,10 @@
-import { IsInt } from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmptyNumber } from '@root/decorators/is-not-empty-number.decorator';
 import { NumberRange } from '@root/decorators/number-range.decorator';
 
 export class SearchPaginationDto {
     @ApiProperty({ description: '페이지' })
-    @IsInt()
+    @IsNotEmptyNumber()
     page: number;
 
     @ApiProperty({ description: '한 페이지 당 조회하고자 하는 페이지의 수' })
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -9,6 +9,7 @@ import { PhoneNumberEntity } from '@root/entities/phone-number.entity';
 import { UserHasPhoneNumberRepository } from '@root/entities/repositories/address-book.repository';
 import { UserRepository } from '@root/entities/repositories/user.repository';
 import { ERROR_MESSAGE } from '@root/utils/error-message';
+import { getSkipAndTake } from '@root/utils/functions/get-skip-and-take.function';
 import * as bcrypt from 'bcrypt';
 import { plainToClass } from 'class-transformer';
 import { In, Not } from 'typeorm';
@@ -156,7 +157,7 @@ export class UserService {
         );
         // 5. userHasPhoneNumberEntity 형태로 가공: [userId, phoneNumberId, phoneNickname]
         const phoneNumbers = [...newSavedPhoneNumbers, ...savedPhoneNumbers];
-        let updatePhoneBook = phoneNumbers.map((phoneNumberEntity) =>
+        const updatePhoneBook = phoneNumbers.map((phoneNumberEntity) =>
             plainToClass(UserHasPhoneNumberEntity, {
                 userId: userId,
                 phoneNumberId: phoneNumberEntity.id,
@@ -175,24 +176,17 @@ export class UserService {
         // return await this.userRepository.update(user, useraddressBookDto)
     }
 
-    async getAcquaintances(userId: number, SearchPaginationDto: SearchPaginationDto) {
+    async getAcquaintances(userId: number, searchPaginationDto: SearchPaginationDto) {
         // 나와 아는 사람(전화번호부 조회, 1다리)
         // [phoneNumberId(전화번호부에 등록된 사람), phoneNickname(저장된 별명)]
-        const bridge = await this.addressBookRepository.find({
-            select: {
-                phoneNumberId: true,
-                phoneNickname: true,
-            },
-            where: {
-                userId,
-            },
-            ...SearchPaginationDto,
+        const bridges = await this.addressBookRepository.find({
+            select: { phoneNumberId: true, phoneNickname: true },
+            where: { userId },
+            ...getSkipAndTake(searchPaginationDto),
         });
 
         // bridge객체의 phoneNumberId: number 중에서 key를 제거하고 Array<Number>형태로 변환
-        const bridgeArray = bridge.map((el) => {
-            return el.phoneNumberId;
-        });
+        const phoneNumberIds = bridges.map((el) => el.phoneNumberId);
 
         // 전화번호 1다리를 건너 아는 사람 조회
         // [userId(한 다리 건넌 사람), phoneNumberId(내 전화번호부에 등록된 사람)]
@@ -203,33 +197,29 @@ export class UserService {
             },
             where: {
                 userId: Not(userId),
-                phoneNumberId: In(bridgeArray),
+                phoneNumberId: In(phoneNumberIds),
             },
         });
         console.log(newFaceIds);
 
-        // 위에서 반환받은 객체를 bridgeArray와 같이 변환
+        // 위에서 반환받은 객체를 phoneNumberIds와 같이 변환
         const newFaceIdArray = newFaceIds.map((el) => {
             return el.userId;
         });
 
         // 한 다리 건넌 사람과 같이 아는 전화번호의 id로 누구를 통했는지 추적
         const chasingNickname = newFaceIds.map((el) => {
-            const target = bridge.find((el_bridge) => {
-                return el_bridge.phoneNumberId == el.phoneNumberId;
-            });
-            return {
-                ...el,
-                bridgeNickname: target.phoneNickname,
-            };
+            const target = bridges.find((bridge) => bridge.phoneNumberId == el.phoneNumberId);
+            return { ...el, bridgeNickname: target.phoneNickname };
         });
-        console.log(chasingNickname);
-        /* 
-      user 객체 조회 후 반환, **미완성**
-      나중에 유저의 프로필과 자기사진(틴더, 글램 등에서는 약 5~6개)을 버킷에 업로드 후,
-      최대 5개까지 저장하는 id -> photoProfile (OneToOne) 릴레이션을 참조하여 앱에서 불러오는 방식?
-      을 업데이트하면 좋을거라 생각했음.
-    */
+
+        /*
+         * user 객체 조회 후 반환, **미완성**
+         * 나중에 유저의 프로필과 자기사진(틴더, 글램 등에서는 약 5~6개)을 버킷에 업로드 후,
+         * 최대 5개까지 저장하는 id -> photoProfile (OneToOne) 릴레이션을 참조하여 앱에서 불러오는 방식?
+         * 을 업데이트하면 좋을거라 생각했음.
+         */
+
         // [id(한 다리 건넌 사람), name, birth]
         const newFaceProfileArray = await this.userRepository.find({
             select: {
@@ -241,23 +231,18 @@ export class UserService {
                 id: In(newFaceIdArray),
             },
         });
-        console.log(newFaceProfileArray);
 
         // 한 다리 건너 누구를 통해 알게 되었는지 내 전화번호부의 번호 별명을 같이 전달.
         const result = newFaceProfileArray.map((el) => {
             const target = chasingNickname.find((el_chase) => {
                 return el_chase.userId == el.id;
             });
-            return {
-                ...el,
-                bridge: target.bridgeNickname,
-            };
+            return { ...el, bridge: target.bridgeNickname };
         });
-        console.log(result);
         return result;
     }
 
-    async getAcquaintancesForTest(userId: number, SearchPaginationDto: SearchPaginationDto) {
+    async getAcquaintancesForTest(userId: number, searchPaginationDto: SearchPaginationDto) {
         /* 
         예시 상황:
             A ~ F가 있을 때,
@@ -276,7 +261,7 @@ export class UserService {
             where: {
                 userId,
             },
-            ...SearchPaginationDto,
+            ...getSkipAndTake(searchPaginationDto),
         });
         console.log('\nbridge:');
         console.log(bridge);
